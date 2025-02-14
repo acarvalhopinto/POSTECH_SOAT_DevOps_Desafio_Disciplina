de01e92455d79a1b174ef48f1583dca84f1d12c7
acarvalhopinto <andrecarvalhopinto@gmail.com>
2025-02-13 21:28:26 -0300

    Update DataController.java

--- src/main/java/com/dev/cicd/data/controller/DataController.java
+++ src/main/java/com/dev/cicd/data/controller/DataController.java
@@ -18,7 +18,7 @@ public class DataController {
 
         @GetMapping("/version")
         public String version() {
-                return "The actual version is 2.1.1";
+                return "The actual version is 3.0.0";
         }
 
         @GetMapping("/nations")
